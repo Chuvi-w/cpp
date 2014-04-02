ef48ce96ce97705427e25738b66baf2e95f471cd
Denis Stepulenok <super.denis@gmail.com>
2014-04-02 21:29:01 +0400

    Отступы и текст

--- lesson_7/QtNetwork/mainwindow.cpp
+++ lesson_7/QtNetwork/mainwindow.cpp
@@ -25,17 +25,17 @@ void MainWindow::UdpChat(QString nick, int port)
         // вызываем метод (слот) read, который
         connect(socket, SIGNAL(readyRead()), this, SLOT(read()));
     } else {
-        qDebug() << "Change port!";
+        qDebug() << "Port " << port << " in use. Change port!";
     }
 }
 
 void MainWindow::on_enterChatButton_clicked()
 {
     if(ui->createServer->isChecked()){
-       UdpChat(ui->nicknameEdit->text(),
-               ui->portNumEdit->text().toInt());
-       send(ui->nicknameEdit->text() +
-            " - создал сервер", USUAL_MESSAGE);
+        UdpChat(ui->nicknameEdit->text(),
+                ui->portNumEdit->text().toInt());
+        send(ui->nicknameEdit->text() +
+             " - создал сервер", USUAL_MESSAGE);
     } else {
         send(ui->nicknameEdit->text() +
              " - вошёл в чат", USUAL_MESSAGE);
@@ -44,68 +44,68 @@ void MainWindow::on_enterChatButton_clicked()
 
 
 void MainWindow::send(QString str, qint8 type) {
-  // Полный пакет данных будет в массиве data
-  QByteArray data;
-  QFile file("test.dat");
-  file.open(QIODevice::WriteOnly);
-  QDataStream out2(&file);
-  // Последовательно выводим в него байты
-  QDataStream out(&data, QIODevice::WriteOnly);
-  out << qint64(0); // Размер сообщения (сначала ставим 0)
-  out << qint8(type); // Тип сообщения
-  out << str; // Само сообщение
-  // Снова перемещаемся на начало массива
-  out.device()->seek(qint64(0));
-  // Записываем в начало сообщения его размер
-  out << qint64(data.size() - sizeof(qint64));
+    // Полный пакет данных будет в массиве data
+    QByteArray data;
+    QFile file("test.dat");
+    file.open(QIODevice::WriteOnly);
+    QDataStream out2(&file);
+    // Последовательно выводим в него байты
+    QDataStream out(&data, QIODevice::WriteOnly);
+    out << qint64(0); // Размер сообщения (сначала ставим 0)
+    out << qint8(type); // Тип сообщения
+    out << str; // Само сообщение
+    // Снова перемещаемся на начало массива
+    out.device()->seek(qint64(0));
+    // Записываем в начало сообщения его размер
+    out << qint64(data.size() - sizeof(qint64));
 
-  /*long long size = sizeof(long long) + sizeof(type) +
+    /*long long size = sizeof(long long) + sizeof(type) +
           str.size();
   unsigned char message[size];
   memcpy(message, size);
   message[sizeof(long long)] = type;
   memcpy() */
 
-  // Отправляем полученный массив данных всем в локальный сети
-  // на порт указанный в интерфейсе
-  socket->writeDatagram(data,
-                        QHostAddress::Broadcast,
-                       ui->portNumEdit->text().toInt() );
+    // Отправляем полученный массив данных всем в локальный сети
+    // на порт указанный в интерфейсе
+    socket->writeDatagram(data,
+                          QHostAddress::Broadcast,
+                          ui->portNumEdit->text().toInt() );
 }
 
 void MainWindow::read() {
-  // Массив для полученных данных
-  QByteArray datagram;
-  // Устанавливаем массиву размер соответствующий размеру полученного пакета данных
-  datagram.resize(socket->pendingDatagramSize());
-  QHostAddress *address = new QHostAddress();
-  socket->readDatagram(datagram.data(), datagram.size(), address);
-  qDebug() << address;
+    // Массив для полученных данных
+    QByteArray datagram;
+    // Устанавливаем массиву размер соответствующий размеру полученного пакета данных
+    datagram.resize(socket->pendingDatagramSize());
+    QHostAddress *address = new QHostAddress();
+    socket->readDatagram(datagram.data(), datagram.size(), address);
+    qDebug() << address;
 
-  // Разбор полученного пакета
-  QDataStream in(&datagram, QIODevice::ReadOnly);
+    // Разбор полученного пакета
+    QDataStream in(&datagram, QIODevice::ReadOnly);
 
-  // Первые 8 байт - размер
-  qint64 size = -1;
-  if(in.device()->size() > sizeof(qint64)) {
-    in >> size;
-  } else return;
-  if (in.device()->size() - sizeof(qint64) < size) return;
+    // Первые 8 байт - размер
+    qint64 size = -1;
+    if(in.device()->size() > sizeof(qint64)) {
+        in >> size;
+    } else return;
+    if (in.device()->size() - sizeof(qint64) < size) return;
 
-  // Получаем тип пакета
-  qint8 type = 0;
-  in >> type;
+    // Получаем тип пакета
+    qint8 type = 0;
+    in >> type;
 
-  if (type == USUAL_MESSAGE) {
-    QString str;
-    in >> str;
-    // Отображаем строчку в интерфейсе
-    ui->plainTextEdit->appendPlainText(str);
-  } else if (type == PERSON_ONLINE) {
-    // Добавление пользователя с считанным QHostAddress //
-  } else if (type == WHO_IS_ONLINE) {
-    send(nickname, qint8(PERSON_ONLINE));
-  }
+    if (type == USUAL_MESSAGE) {
+        QString str;
+        in >> str;
+        // Отображаем строчку в интерфейсе
+        ui->plainTextEdit->appendPlainText(str);
+    } else if (type == PERSON_ONLINE) {
+        // Добавление пользователя с считанным QHostAddress //
+    } else if (type == WHO_IS_ONLINE) {
+        send(nickname, qint8(PERSON_ONLINE));
+    }
 }
 
 void MainWindow::on_sendButton_clicked()
