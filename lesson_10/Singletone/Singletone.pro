030951d3f828457133a89ba77c864a7a5e1bea1b
Denis Stepulenok <super.denis@gmail.com>
2014-04-15 20:44:06 +0400

    Пример Singletone

---
+++ lesson_10/Singletone/Singletone.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-04-15T20:33:16
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Singletone
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ lesson_10/Singletone/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+    static int count;
+    int id;
+    A(){
+        id = ++count;
+        cout << "Constructor A " << id << endl;
+    }
+    static A* a;
+public:
+    ~A(){
+        if(a == NULL)
+            return;
+        cout << "Destructor A " << id << endl;
+        a = NULL;
+    }
+    static A& instance(){
+        if(a == NULL){
+            a = new A();
+        }
+        return *a;
+    }
+    void show(){
+        cout << "A id = " << id << endl;
+    }
+};
+
+int A::count = 0;
+A* A::a = NULL;
+
+int main()
+{
+    A a = A::instance(),
+      b = A::instance();
+    a.show();
+    b.show();
+    return 0;
+}
