7af3bab7bd86e9a6b2eb85b5810464b95b191d07
Denis Stepulenok <super.denis@gmail.com>
2014-05-06 20:28:27 +0400

    Изменения на занятии 2

--- lesson_2/min/main.c
+++ lesson_2/min/main.c
@@ -1,7 +1,18 @@
+// Универсальная функция min на C
 #include <stdio.h>
 #include <stdlib.h>
 
+// Макрос с использованием тернарного оператора
 #define MIN(a,b) (a < b) ? a : b
+
+int min(int a, int b){
+    return (a < b) ? a : b;
+}
+
+// Нужен префикс f:  math.h -> abs/fabs
+double fmin(double a, double b){
+    return (a < b) ? a : b;
+}
 
 int main()
 {
