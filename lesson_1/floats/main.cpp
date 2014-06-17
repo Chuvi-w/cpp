1b111d383f4e13cec3f84f239b959eadfa5ca719
Denis Stepulenok <super.denis@gmail.com>
2014-06-17 22:13:19 +0400

    Save changes

--- lesson_1/floats/main.cpp
+++ lesson_1/floats/main.cpp
@@ -8,18 +8,19 @@ int main()
 {
     float f = 0.023; // 4 байта
 
+    long double ld = 0.12; // 10 байт
+
     double a = 0.1; // 8 байт
     double b = 0.9;
     double c = 1;
 
-    long double ld = 0.12; // 10 байт
-
+//    if(a + b == c) {
+    // fabs - взять по модулю
     if( fabs((a + b) - c) < 0.001)   {
       cout << a << " + " << b << " == " << c << endl;
     } else {
       cout << a << " + " << b << " != " << c << endl;
     }
-
 
     return 0;
 }
--- lesson_1/if_error/main.cpp
+++ lesson_1/if_error/main.cpp
@@ -4,16 +4,25 @@
 
 using namespace std;
 
+#define MY_CONST 3
+
 int main()
 {
-  int i = 0, j;
-  if(i == 0){
-    cout << "True!" << endl;
+  int a = 2, b = 3;
+  if(MY_CONST == a){
+     cout << "a == b" << endl;
   } else {
-    cout << "False!" << endl;
+     cout << "a != b" << endl;
   }
 
-  if(j = (i = 5)){  // Использование присваивания "=" вместо "==" 
+  int i = 0, j;
+  if(i == 0){
+    cout << "True! i = 0" << endl;
+  } else {
+    cout << "False! i != 0" << endl;
+  }
+
+  if(j = (i = 5)){  // Использование присваивания "=" вместо "=="
     cout << "True!" << endl;
   } else {
     cout << "False!" << endl;
