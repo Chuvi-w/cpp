c6437f3b321098d85e47d0abdc125560e9b0deec
Denis Stepulenok <super.denis@gmail.com>
2014-07-01 19:57:47 +0400

    Save changes

--- lesson_3/HomeWork/main.cpp
+++ lesson_3/HomeWork/main.cpp
@@ -2,18 +2,56 @@
 
 using namespace std;
 
+// Элемент стека и очереди
+struct E {
+  int value; // Значение элемента
+  E* next; // Указатель на следующий элемент
+};
+
+
 struct Stack {
+  E* top; // Вершина стека
+  // Конструктор
+  Stack() : top(NULL) {}
   // Значение поместить на вершину стека
   void push(int value){
+    E* e = new E;
+    e->value = value;
+    e->next = top;
+    top = e;
   }
   // Взять значение с вершины стека
   int pop(){
+    E* e = top;
+    int value = e->value;
+    top = top->next;
+    delete e;
+
+    return value;
   }
 };
 
 struct Queue {
+  E* start;
+  // Конструктор
+  Queue() : start(NULL) {};
   // В конец очереди
   void put(int value){
+    // Создаём новый элемент
+    E *e = new E;
+    e->value = value;
+    e->next = NULL;
+    // Если очередь пуста
+    if(start = NULL){
+      start = e;
+    } else {
+      E *last = start;
+      while(last->next != NULL)
+        last = last->next;
+      // К последнему элементу добавляем
+      // новый элемент
+      last->next = e;
+    }
   }
   // Забрать первый элемент из очереди
   int get(){
