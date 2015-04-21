ee84011a8efc89525ab75a6b53059e9b352f5983
Denis Stepulenok <super.denis@gmail.com>
2015-04-21 17:07:31 +0300

    Создание класса в C++ по его идентификатору

--- lesson_07/02_AbstractFactory/main.cpp
+++ lesson_07/02_AbstractFactory/main.cpp
@@ -1,9 +1,75 @@
+/// Создание класса в C++ по его идентификатору
+/// -------------------------------------------
 #include <QCoreApplication>
+#include <iostream>
+#include <typeinfo>
 
-struct MyClass {};
-struct MyClass1 : public MyClass {};
-struct MyClass2 : public MyClass {};
+using namespace std;
 
+/// Иерархия классов
+///-->
+struct MyClass {
+  virtual void show() = 0;
+};
+
+struct MyClass1 : public MyClass {
+  MyClass1() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+  void show() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+};
+
+struct MyClass2 : public MyClass {
+  MyClass2() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+  void show() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+};
+
+///<--
+
+/// Первый способ
+/// -------------
+/// Функция - фабрика
+///-->
+MyClass* CreateInstance(int id) {
+  switch(id) {
+    case 1:
+      return new MyClass1();
+
+    case 2:
+      return new MyClass2();
+
+    default:
+      cout << "TODO: " << __FUNCTION__ << " for " << id << endl;
+  }
+
+  throw id;
+}
+///<--
+
+void v1() {
+  /// Использование
+  ///-->
+  MyClass* a = CreateInstance(1);
+  a->show();
+  MyClass* b = CreateInstance(2);
+  b->show();
+  ///<--
+}
+
+/// Второй способ
+/// -------------
+/// Спецификация шаблона для определённых значений
+///-->
+template<int id>
+MyClass* Create() {
+  throw "Need specialization";
+}
 
 template<>
 MyClass* Create<1>() {
@@ -14,9 +80,19 @@ template<>
 MyClass* Create<2>() {
   return new MyClass2();
 }
+///<--
 
-int main(int argc, char* argv[]) {
-  QCoreApplication a(argc, argv);
+/// Использование
+void v2() {
+  ///-->
+  MyClass* a1 = Create<1>(), *a2 = Create<2>();
+  a1->show();
+  a2->show();
+  ///<--
+}
 
-  return a.exec();
+int main() {
+  v1();
+  v2();
+  return 0;
 }
--- lesson_07/README.md
+++ lesson_07/README.md
@@ -60,6 +60,91 @@ extern int globalVar;
 
 [01_extern_vars\vars.h](01_extern_vars\vars.h)
 
+Создание класса в C++ по его идентификатору
+-------------------------------------------
+Иерархия классов
+``` cpp
+struct MyClass {
+  virtual void show() = 0;
+};
+
+struct MyClass1 : public MyClass {
+  MyClass1() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+  void show() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+};
+
+struct MyClass2 : public MyClass {
+  MyClass2() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+  void show() {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+};
+
+```
+
+Первый способ
+-------------
+Функция - фабрика
+``` cpp
+MyClass* CreateInstance(int id) {
+  switch(id) {
+    case 1:
+      return new MyClass1();
+
+    case 2:
+      return new MyClass2();
+
+    default:
+      cout << "TODO: " << __FUNCTION__ << " for " << id << endl;
+  }
+
+  throw id;
+}
+```
+
+Использование
+``` cpp
+  MyClass* a = CreateInstance(1);
+  a->show();
+  MyClass* b = CreateInstance(2);
+  b->show();
+```
+
+Второй способ
+-------------
+Спецификация шаблона для определённых значений
+``` cpp
+template<int id>
+MyClass* Create() {
+  throw "Need specialization";
+}
+
+template<>
+MyClass* Create<1>() {
+  return new MyClass1();
+}
+
+template<>
+MyClass* Create<2>() {
+  return new MyClass2();
+}
+```
+
+Использование
+``` cpp
+  MyClass* a1 = Create<1>(), *a2 = Create<2>();
+  a1->show();
+  a2->show();
+```
+
+[02_AbstractFactory\main.cpp](02_AbstractFactory\main.cpp)
+
 Работа с QString
 ----------------
 QString("%1 - %2").arg(s1).arg(s2)
